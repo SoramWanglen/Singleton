1a6bb43ba0b5777064760fae9dd67aa8eee4ddeb
SoramWanglen <soramwanglen.net@gmail.com>
2022-11-29 15:43:27 +0530

    files added for singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿//Thread Safety Singleton using Double-Check Locking
+using System;
+
+namespace Singleton
+{
+    public sealed class Program
+    {
+        private Program()
+        {
+        }
+        private static readonly Lazy<Program> lazy = new Lazy<Program>(() => new Program());
+        public static Program Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+}
